f114b191b05b1bc3fe862c24554f25b7c67829a2
Oytizzo <oytizzo@gmail.com>
2021-05-08 21:14:37 +0600

    login View added

--- src/dj_proj/urls.py
+++ src/dj_proj/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from users import views as users_views
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('register/', users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
