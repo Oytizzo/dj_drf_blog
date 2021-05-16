8ace9435bebf626311bf86929533d33751542725
Oytizzo <oytizzo@gmail.com>
2021-05-16 23:51:10 +0600

    Add PostCreateView and fixed two bugs
    After adding PostCreateView Integrity Error raised and fixed by definig form_valid in PostCreateView and then Improperly Configured Error raised and fixed by redirecting to PostDetailView after post creation

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
 ]
