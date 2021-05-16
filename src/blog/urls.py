69b2f62982832fb8101e0b034150577cc7f10403
Oytizzo <oytizzo@gmail.com>
2021-05-16 23:21:53 +0600

    Add PostDetailView

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -25,3 +25,7 @@ class PostListView(ListView):
     # default context object name 'object_list' and '<model>_list'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
