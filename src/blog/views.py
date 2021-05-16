028062e2ce2c13d76803cbdd120abb06e664e4e1
Oytizzo <oytizzo@gmail.com>
2021-05-16 22:36:58 +0600

    Clean and initiate class based views also CRUD posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '2021-05-01'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '2021-05-02'
-    }
-]
-
 
 def home(request):
     context = {
