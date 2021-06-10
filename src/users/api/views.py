c1b7478ece632a586eccb55fb7d7247f541fb275
Oytizzo <oytizzo@gmail.com>
2021-06-10 17:14:30 +0600

    Add users registers via api

--- src/users/api/views.py
+++ src/users/api/views.py
@@ -1,20 +1,36 @@
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+#
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+#
+# from ..forms import UserRegisterForm
+#
+#
+# @api_view(['GET', 'POST'])
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Hi, {username}, Your account has been created! Now you can log in!')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     return render(request, 'users/register.html', {'form': form})
 
-from django.shortcuts import render, redirect
-from django.contrib import messages
 
-from ..forms import UserRegisterForm
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
 
 
-@api_view(['GET', 'POST'])
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi, {username}, Your account has been created! Now you can log in!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+class CreateUserView(CreateAPIView):
+
+    model = get_user_model()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
