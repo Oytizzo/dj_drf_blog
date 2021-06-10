b7b6a67b2b8b2c2b4557a090413ab1c27dab9d3d
Oytizzo <oytizzo@gmail.com>
2021-06-10 17:15:40 +0600

    Update users.api.views

--- src/users/api/views.py
+++ src/users/api/views.py
@@ -1,25 +1,3 @@
-# from rest_framework.decorators import api_view
-#
-# from django.shortcuts import render, redirect
-# from django.contrib import messages
-#
-# from ..forms import UserRegisterForm
-#
-#
-# @api_view(['GET', 'POST'])
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             messages.success(request, f'Hi, {username}, Your account has been created! Now you can log in!')
-#             return redirect('login')
-#     else:
-#         form = UserRegisterForm()
-#     return render(request, 'users/register.html', {'form': form})
-
-
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model
